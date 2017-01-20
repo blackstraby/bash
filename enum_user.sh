cd9e567ffebcc71cf2e5aad2cc14e6fb6f0274d7
Jonas Gomes <rockerskater66@gmail.com>
2017-01-20 21:55:33 -0200

    Update enum_user.sh

--- enum_user.sh
+++ enum_user.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-		##############################################
-		#	@@Author: Black Straby				          	 #
-		#   @@Date: June 13th, 2016 				         #
-		#	@@Function: Extrair usuarios do sistema	   #
-		##############################################
+		##################################################
+		#	@@Author: Black Straby		  	 #
+		#   @@Date: June 13th, 2016 		         #
+		#	@@Function: Extrair usuarios do sistema	 #
+		##################################################
 
 usr=( $(cut -d: -f1 /etc/passwd) )
 
